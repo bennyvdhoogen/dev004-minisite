175dedc2541186c56246672c9e10b3e984626b3d
benjaminluck <bennyvandenhoogen@192.168.2.34>
2017-05-16 02:34:15 +0200

    Made Soundcloud widget hidden

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
           <button id="mute-btn" onclick="mutePlayer(this);" class="mute-btn mute-btn-unmuted"></button>
           <div id="mute-btn-muted" class="mute-btn mute-btn-muted hidden"></div>
         </div>
-        <iframe width="100%" height="450"  scrolling="no" frameborder="no" id="soundcloud-player"
+        <iframe width="100%" height="450"  style="display:none" scrolling="no" frameborder="no" id="soundcloud-player"
                 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/304658516&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
         </iframe>
 
