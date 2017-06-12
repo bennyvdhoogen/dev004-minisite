55bce1d64d56265dae051e66c36b580d61105fbe
benjaminluck <bennyvandenhoogen@bennys-mbp.home>
2017-06-12 23:34:29 +0200

    Updated StateManager to now wait 1 second to removing the splash screen after all assets have been loaded. This should give the browser enough time to use the display meshes, calculate shaders and attach texture and should prevent the background image flashing at startup. Also added the artwork instead of ‘loading’.

--- templates/loader.php
+++ templates/loader.php
@@ -4,8 +4,9 @@
       <div id="splash-information-panel" class="col-xs-12 col-md-12" style="z-index:30000; height:100vh; background-color: white;">
         <div class="row">
           <div class="centered-information">
+            <img src="assets/svg/cover_invert_1.svg" class="animate-flicker" width="300px" style="max-width: 80vw;">
             <div class="loader">
-              <h1>loading..</h1>
+              <!-- <h1>loading..</h1> -->
             </div>
           </div>
         </div>
