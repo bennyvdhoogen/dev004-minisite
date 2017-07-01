400601e9569b35df07afce4d58fefff01ce968cc
benjaminluck <bennyvandenhoogen@bennys-mbp.home>
2017-07-02 00:56:14 +0200

    Alternative scroll mechanic using left and right speeds instead of a direction variable.

--- js/src/components/ScrollingController.js
+++ js/src/components/ScrollingController.js
@@ -2,28 +2,37 @@ var ScrollingController = (function () {
 	var self = {};
   // idle, playing, muted
   self.scrollingCurrentSpeed = 0;
-  self.scrollingActualDirection = 0;
-  self.scrollingTargetDirection = 0;
-  self.scrollingIncrement = 0.001;
+	self.scrollingLeftSpeed = 0;
+	self.scrollingRightSpeed = 0;
+
+  self.scrollingIncrement = 0.01;
   self.dampingFactor = self.scrollingIncrement * 1;
   self.scrollingStartSpeed = 0.01;
-  self.scrollingCap = 0.3;
-  self.scrollDuration = 100; //in ms
+  self.scrollingCap = 0.01;
+  self.scrollDuration = 2000; //in ms
   self.scrollStartTime = {};
+	self.scrollTimeLeft = 0;
+	self.scrollEndTime;
   self.isScrolling = false;
   self.isStopping = false;
 
   // on scroll up/down event:
-  // 1) scroll speed increases/decreases
-  // 2) direction changes to 1
+  // 1) scroll left speed increases (if event.deltaY < 0)
+	// 1) scroll right speed increases (if event.deltaY > 0)
   // every frame the scroll speed decreases so that after {{scrollDuration}} the speed is 0
 
   self.determineTargetDirection = function(event){
-    if(event.deltaY > 0){
-      self.scrollingTargetDirection = 1;
-    }
     if(event.deltaY < 0){
-      self.scrollingTargetDirection = -1;
+			self.scrollingTargetDirection = -1;
+			if(self.scrollingLeftSpeed < self.scrollingCap){
+				self.scrollingLeftSpeed = self.scrollingLeftSpeed + (self.scrollingIncrement);
+			}
+    }
+		if(event.deltaY > 0){
+      self.scrollingTargetDirection = 1;
+			if(self.scrollingRightSpeed < self.scrollingCap){
+				self.scrollingRightSpeed = self.scrollingRightSpeed + (self.scrollingIncrement);
+			}
     }
   }
 
@@ -32,6 +41,7 @@ var ScrollingController = (function () {
 
     if(!self.isScrolling){
       self.scrollStartTime = new Date().getTime();
+			self.scrollEndTime = self.scrollStartTime + self.scrollDuration;
       self.scrollingCurrentSpeed = self.scrollingStartSpeed;
       self.isScrolling = true;
     }else{
@@ -39,65 +49,24 @@ var ScrollingController = (function () {
     }
   }
 
-  self.increaseSpeed = function(){
-    if(self.scrollingCurrentSpeed < self.scrollingCap){
-      self.scrollingCurrentSpeed = self.scrollingCurrentSpeed + (self.scrollingIncrement);
-    }
-    // if direction is the same, speed ++ until cap
-    // if direction different: speed -- untill 0
-    // if direciton different: when speed >= 0 change actualDirection
-  };
-
-  self.decreaseSpeed = function(){
-    if(self.scrollingCurrentSpeed > 0){
-     self.scrollingCurrentSpeed = self.scrollingCurrentSpeed - (self.scrollingIncrement);
-    }
-  }
-
-  self.updateDirection = function(){
-    if(self.scrollingActualDirection == 0){
-      self.scrollingActualDirection = self.scrollingTargetDirection;
-    }
-
-    if(self.scrollingActualDirection != self.scrollingTargetDirection){
-    //  debugger;
-      console.log('change direction!');
-      self.scrollingCurrentSpeed = 0;
-      if(self.scrollingCurrentSpeed <= 0){
-        // change direction if speed hits 0
-        self.scrollingActualDirection = self.scrollingActualDirection * -1;
-      }
-    }
-  }
-
   self.updateSpeed = function(){
-    if(self.isScrolling){
-      if(self.scrollingTargetDirection == self.scrollingActualDirection){
-        self.increaseSpeed();
-      }
-
-      if(self.scrollingTargetDirection != self.scrollingActualDirection){
-        self.decreaseSpeed();
-      }
-      if(self.scrollingCurrentSpeed > self.scrollingCap){
-        self.scrollingCurrentSpeed = self.scrollingCap;
-      }
-
-    }else{
-      self.decreaseSpeed();
-    }
-
-
-    if(self.scrollingCurrentSpeed < 0){
-      self.scrollingCurrentSpeed = 0;
-    }
-
+    self.scrollingCurrentSpeed = (self.scrollingLeftSpeed * -1) + self.scrollingRightSpeed;
   }
 
   self.applyDamping = function(){
-    if(self.scrollingCurrentSpeed > 0){
-     self.scrollingCurrentSpeed = self.scrollingCurrentSpeed - (self.dampingFactor);
-    }
+
+
+    if(self.scrollingLeftSpeed > 0){
+     self.scrollingLeftSpeed  = self.scrollingLeftSpeed  - (self.dampingFactor);
+		 }else{
+			 self.scrollingLeftSpeed  = 0;
+		 }
+		if(self.scrollingRightSpeed > 0){
+     self.scrollingRightSpeed  = self.scrollingRightSpeed  - (self.dampingFactor);
+		 }else{
+			 self.scrollingRightSpeed  = 0;
+		 }
+
     if(self.scrollingCurrentSpeed < 0){
       self.scrollingActualDirection = 0;
       self.scrollingTargetDirection = 0;
@@ -106,6 +75,7 @@ var ScrollingController = (function () {
 
   self.updateTimeElapsed = function(){
     if(self.isScrolling){
+			self.scrollTimeLeft = self.scrollEndTime - new Date().getTime();
       if(self.scrollStartTime < (new Date().getTime() - self.scrollDuration)){
         self.applyDamping();
         self.isScrolling = false;
@@ -115,12 +85,17 @@ var ScrollingController = (function () {
 
   self.applyScrollRotation = function(){
     self.updateTimeElapsed();
-    console.log(self.scrollingCurrentSpeed);
-    console.log(self.isScrolling);
-      window.controls.rotate(self.scrollingCurrentSpeed * self.scrollingActualDirection);
-      self.updateDirection();
+      window.controls.rotate(self.scrollingCurrentSpeed);
+
       self.updateSpeed();
       self.applyDamping();
+
+			console.log("isScrolling : "+ self.isScrolling);
+			console.log("scrollStart : "+ self.scrollStartTime);
+			console.log("scrollTimeLeft :" + self.scrollTimeLeft);
+			console.log("scrollEnd : "+ (self.scrollEndTime));
+			console.log("left : " + self.scrollingLeftSpeed * 1);
+			console.log("right : " + self.scrollingRightSpeed * 1);
   }
 
 	return self;
